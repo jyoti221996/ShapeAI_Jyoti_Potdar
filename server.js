c219cedc9319c6e2d38b290c7efcfe9b872d3081
jyoti221996 <71517430+jyoti221996@users.noreply.github.com>
2021-05-17 11:24:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get("/" , function(request, response){
 
 app.post("/" , function(request, response){
 
-  var weight = Number(request.body.weight);
-  var height = Number(request.body.height);
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
 
   var result = (weight/(height * height));
 
